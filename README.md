f200971df61c61df5b5b4ede35d8595ae385ff7c
cartechr <54558708+cartechr@users.noreply.github.com>
2019-11-06 16:47:36 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# 09-Juicy-Breakout
-A much better breakout game
+I have completed everything that I was asked to complete
+1)I added scoring systems that adds up the score based off of which and what types of tiles are hit by the ball.
+3)The player has 3 lives
+    3a)If the ball falls, the player has to left click to get the ball back.
+4)I have spent a decent amount of time on the arrangement of the tiles, and I am very proud of this achievement.
+5)I kept messing up on the first repository that I created (08-Breakout), so I decided to start fresh and created "08-Breakout-01."
---
+++ Scripts/paddle.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var new_ball = preload("res://Scenes/Ball.tscn")
+
+func _ready():
+ set_process_input(true)
+
+func _physics_process(delta):
+ var mouse_x = get_viewport().get_mouse_position().x
+ position = Vector2(mouse_x, position.y)
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		print("Clicking")
+		if not get_parent().has_node("Ball"):
+			print("Creating a new ball")
+			var ball = new_ball.instance()	
+			ball.position = position - Vector2(0, 32)
+			ball.name = "Ball"
+			ball.linear_velocity = Vector2(200, -200)
+			get_parent().add_child(ball)
